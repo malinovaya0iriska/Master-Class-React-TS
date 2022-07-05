bc1a820972cec961affac8528c2f33e2c7b94a51
Irina Matrtsulevich <malinovayairiska@gmail.com>
2022-07-05 17:01:31 +0300

    feat: add reusable Input

---
+++ src/ui-components/Input/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { InputProps } from './types';
+import './style.css';
+
+export const Input: React.FC<InputProps> = ({
+  onChange,
+  error,
+  positive,
+  inputStyle,
+  inputContainerStyle,
+  label,
+  inputRef,
+}) => {
+  const overrideClassName = (error && 'error-ui') || (positive && 'positive-ui') || '';
+
+  return (
+    <div style={inputContainerStyle} className="input-container">
+      <div className="label">{label}</div>
+      <input
+        ref={inputRef}
+        style={inputStyle}
+        onChange={onChange}
+        className={overrideClassName}
+      />
+      {error && <div className="error-message">{error}</div>}
+    </div>
+  );
+};
